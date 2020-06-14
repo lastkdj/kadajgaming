fd30b402e4ebb7e0fe91ec6abc9d32288251474c
Kadaj <64706671+lastkdj@users.noreply.github.com>
2020-06-14 13:58:24 -0400

    Segundo Cambio

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Mutherfucker
+          Learn React Mutherfucker Again
         </a>
       </header>
     </div>
