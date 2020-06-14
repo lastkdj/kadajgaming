48fa829d16591fe35caa6b3ded51271f05395d82
Kadaj <64706671+lastkdj@users.noreply.github.com>
2020-06-14 14:05:20 -0400

    Updates

---
+++ precache-manifest.cdda32544297986113b7b7d3edb49558.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4158aa86eec73f52e3c801af2f843617",
+    "url": "/kadajgaming/index.html"
+  },
+  {
+    "revision": "30b351318dd9c7097020",
+    "url": "/kadajgaming/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c928957a2014c8fa1496",
+    "url": "/kadajgaming/static/js/2.63b43018.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kadajgaming/static/js/2.63b43018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30b351318dd9c7097020",
+    "url": "/kadajgaming/static/js/main.6d84885d.chunk.js"
+  },
+  {
+    "revision": "bfb06f68744b24ec6bb0",
+    "url": "/kadajgaming/static/js/runtime-main.dce8df2e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/kadajgaming/static/media/logo.5d5d9eef.svg"
+  }
+]);
